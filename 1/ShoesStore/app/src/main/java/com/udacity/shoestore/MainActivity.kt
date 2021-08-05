e68089d83f63ab2d270c13efcfa78618006b6c2b
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 21:39:20 +0800

    replace fragment with FragmentContainerView

--- 1/ShoesStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ 1/ShoesStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,7 +3,9 @@ package com.udacity.shoestore
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
@@ -12,6 +14,7 @@ import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +22,10 @@ class MainActivity : AppCompatActivity() {
         Timber.plant(Timber.DebugTree())
 
         setSupportActionBar(binding.toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.myNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
 
-
-        val navController = this.findNavController(R.id.myNavHostFragment)
         val appBarConfig = AppBarConfiguration(navController.graph)
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfig)
     }
