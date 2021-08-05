17ededfc4d3bdaead4a15a9c2414110d038ccb9c
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-06 00:42:55 +0800

    unit test with live data (with util extension)

--- 4/ToDo/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ 4/ToDo/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,10 +1,9 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.Event
+import getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.not
 import org.hamcrest.Matchers.nullValue
@@ -22,24 +21,42 @@ class TasksViewModelTest{
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
+    // BEFORE USING LiveDataTestUtil.kt
+//    @Test
+//    fun addNewTask_setsNewTaskEvent() {
+//        // Given a fresh ViewModel
+//        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+//
+//        // Create observer - no need for it to do anything!
+//        val observer = Observer<Event<Unit>>{}
+//        try{
+//            // When adding a new task
+//            tasksViewModel.addNewTask()
+//
+//            // Then the new task event is triggered
+//            val value = tasksViewModel.newTaskEvent.value
+//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+//        } finally {
+//            // whatever happens, don't forgot to remove the observer
+//            tasksViewModel.newTaskEvent.removeObserver(observer)
+//        }
+//    }
+
+    // AFTER USING LiveDataTestUtil.kt
     @Test
     fun addNewTask_setsNewTaskEvent() {
+
         // Given a fresh ViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-        // Create observer - no need for it to do anything!
-        val observer = Observer<Event<Unit>>{}
-        try{
-            // When adding a new task
-            tasksViewModel.addNewTask()
+        // When adding a new task
+        tasksViewModel.addNewTask()
 
-            // Then the new task event is triggered
-            val value = tasksViewModel.newTaskEvent.value
-            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
-        } finally {
-            // whatever happens, don't forgot to remove the observer
-            tasksViewModel.newTaskEvent.removeObserver(observer)
-        }
+        // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(
+            value.getContentIfNotHandled(), (not(nullValue()))
+        )
     }
 
 
