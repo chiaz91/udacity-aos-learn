7236cf6f285fe4255f8277d1a3568bc474b6a662
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 21:19:09 +0800

    encapsulation with backing property *

--- 1/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ 1/GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,23 +1,28 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
 class GameViewModel: ViewModel(){
     // The current word
-    var word = MutableLiveData<String>()
+    var _word = MutableLiveData<String>()
+    val word: LiveData<String>
+        get() = _word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init{
         Log.i("GameViewModel", "GameViewModel created!")
-        score.value=0
+        _score.value=0
 
         resetList()
         nextWord()
@@ -38,17 +43,17 @@ class GameViewModel: ViewModel(){
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (_score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (_score.value)?.plus(1)
         nextWord()
     }
 
