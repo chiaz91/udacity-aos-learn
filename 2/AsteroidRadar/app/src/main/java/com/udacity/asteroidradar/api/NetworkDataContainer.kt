30c9f46dc33488f79a0a55ccc18b7f037ca42709
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:46:10 +0800

    rollback

--- 2/AsteroidRadar/app/src/main/java/com/udacity/asteroidradar/api/NetworkDataContainer.kt
+++
@@ -1,9 +0,0 @@
-package com.udacity.asteroidradar.api
-
-
-/*
- * DataTransferObjects are responsible for parsing responses from the server or formatting objects
- * to send to the server. You should convert these to domain objects before using them.
- */
-
-// TODO: define temp data object later
