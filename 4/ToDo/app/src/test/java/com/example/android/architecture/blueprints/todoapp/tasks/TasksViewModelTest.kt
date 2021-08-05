f3546ea51917c7ce17ee10daa5314f8f2434300d
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-06 00:45:15 +0800

    move repeated code to before block

--- 4/ToDo/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ 4/ToDo/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -6,12 +6,23 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import getOrAwaitValue
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest{
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+    /* Since all of these tests are going to test the TasksViewModel and will need a view model,
+       it's safe to move this code to a @Before block
+     */
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
 
     /* Testing live data
        add following rule to allow architecture components related background jobs in the same thread
@@ -20,34 +31,10 @@ class TasksViewModelTest{
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    // BEFORE USING LiveDataTestUtil.kt
-//    @Test
-//    fun addNewTask_setsNewTaskEvent() {
-//        // Given a fresh ViewModel
-//        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-//
-//        // Create observer - no need for it to do anything!
-//        val observer = Observer<Event<Unit>>{}
-//        try{
-//            // When adding a new task
-//            tasksViewModel.addNewTask()
-//
-//            // Then the new task event is triggered
-//            val value = tasksViewModel.newTaskEvent.value
-//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
-//        } finally {
-//            // whatever happens, don't forgot to remove the observer
-//            tasksViewModel.newTaskEvent.removeObserver(observer)
-//        }
-//    }
 
     // AFTER USING LiveDataTestUtil.kt
     @Test
     fun addNewTask_setsNewTaskEvent() {
-
-        // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         // When adding a new task
         tasksViewModel.addNewTask()
 
@@ -60,10 +47,6 @@ class TasksViewModelTest{
 
     @Test
     fun setFilterAllTasks_tasksAddViewVisible() {
-
-        // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         // When the filter type is ALL_TASKS
         tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
 
