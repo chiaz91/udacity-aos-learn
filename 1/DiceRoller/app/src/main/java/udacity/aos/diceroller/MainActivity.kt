782454de9b968dd29086f94436c70328312f4144
chiaz91 <chiayu.dev@gmail.com>
2021-08-05 19:51:26 +0800

    converting to image view

--- 1/DiceRoller/app/src/main/java/udacity/aos/diceroller/MainActivity.kt
+++ 1/DiceRoller/app/src/main/java/udacity/aos/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package udacity.aos.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
@@ -19,7 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice(){
         val randomInt = Random().nextInt(6) + 1
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
