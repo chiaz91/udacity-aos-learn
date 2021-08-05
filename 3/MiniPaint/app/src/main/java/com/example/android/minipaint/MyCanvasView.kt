cc88726935a8e6468d1b414dae03e0f0878e4135
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 23:47:04 +0800

    prepare variable needed for drawing

--- 3/MiniPaint/app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ 3/MiniPaint/app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -3,14 +3,35 @@ package com.example.android.minipaint
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
+private const val STROKE_WIDTH = 12f // has to be float
 class MyCanvasView(context: Context) : View(context) {
     // caching for drawing history
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    // canvas drawing needed
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+    // Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        style = Paint.Style.STROKE // default: FILL
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
+    // to store the path that is being drawn when following the user's touch on the screen
+    private var path = Path()
+
 
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
         super.onSizeChanged(width, height, oldWidth, oldHeight)
