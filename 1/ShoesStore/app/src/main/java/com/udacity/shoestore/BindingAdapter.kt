b8068218da03de894f0a322624eada4cecc7d55e
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 21:41:41 +0800

    binding adapter for inflating shoes item

---
+++ 1/ShoesStore/app/src/main/java/com/udacity/shoestore/BindingAdapter.kt
@@ -0,0 +1,22 @@
+package com.udacity.shoestore
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.RowShoeBinding
+import com.udacity.shoestore.models.Shoe
+
+
+@BindingAdapter("listShoes")
+fun bindRecyclerView(container: LinearLayout, data: List<Shoe>?) {
+    val inflater = container.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    container.removeAllViews()
+    data?.forEach{ shoe ->
+        val itemBinding: RowShoeBinding = DataBindingUtil.inflate(
+            inflater, R.layout.row_shoe, container, false)
+        itemBinding.curShoe = shoe
+        container.addView(itemBinding.root)
+    }
+}
--- 1/ShoesStore/app/src/main/java/com/udacity/shoestore/fragments/ShoeListFragment.kt
+++ 1/ShoesStore/app/src/main/java/com/udacity/shoestore/fragments/ShoeListFragment.kt
@@ -2,16 +2,13 @@ package com.udacity.shoestore.fragments
 
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
-import com.udacity.shoestore.models.Shoe
 import com.udacity.shoestore.models.ShoeViewModel
 
 
@@ -25,31 +22,16 @@ class ShoeListFragment : Fragment() {
     ): View? {
         setHasOptionsMenu(true)
 
+        // retrieving view mode
+        viewModel = ViewModelProvider(requireActivity()).get(ShoeViewModel::class.java)
+
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         binding.fab.setOnClickListener{
             it.findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToShoeDetailsFragment())
         }
-
-        // retrieving view mode
-        viewModel = ViewModelProvider(requireActivity()).get(ShoeViewModel::class.java)
-        viewModel.shoes?.observe(viewLifecycleOwner, Observer { shoes ->
-            // clear previous inflated views
-            binding.shoesContainer.removeAllViews()
-
-            // manually create create item views
-            if (shoes.size == 0){
-                val emptyView = createEmptyView()
-                binding.shoesContainer.addView(emptyView)
-            } else {
-                shoes.forEach{ shoe ->
-                    val itemView = createItemViewForShoe(shoe)
-                    binding.shoesContainer.addView(itemView)
-                }
-            }
-
-            binding.invalidateAll()
-        })
 
         return binding.root
     }
@@ -68,23 +50,5 @@ class ShoeListFragment : Fragment() {
 
     private fun logout(){
         findNavController().navigate(ShoeListFragmentDirections.actionShoeListFragmentToLoginFragment())
-    }
-
-    private fun createEmptyView(): View {
-        val empty = TextView(requireContext())
-        empty.text = "No shoes found"
-        return empty
-    }
-
-    private fun createItemViewForShoe(shoe: Shoe): View{
-        val itemView = layoutInflater.inflate(R.layout.row_shoe, binding.shoesContainer,false)
-        with(itemView){
-            // todo: extract for string resource
-            findViewById<TextView>(com.udacity.shoestore.R.id.name).text = shoe.name
-            findViewById<TextView>(com.udacity.shoestore.R.id.size).text = "Size: ${shoe.size}"
-            findViewById<TextView>(com.udacity.shoestore.R.id.company).text = "Company: ${shoe.company}"
-            findViewById<TextView>(com.udacity.shoestore.R.id.description).text = "Description: ${shoe.description}"
-        }
-        return itemView
     }
 }
