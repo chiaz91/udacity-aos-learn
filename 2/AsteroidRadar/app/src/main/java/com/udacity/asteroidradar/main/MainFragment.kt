1b0774b00b761ae9f000d45bd69a6079c8bb8424
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:49:27 +0800

    loading picture of the day

--- 2/AsteroidRadar/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
+++ 2/AsteroidRadar/app/src/main/java/com/udacity/asteroidradar/main/MainFragment.kt
@@ -1,16 +1,19 @@
 package com.udacity.asteroidradar.main
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.squareup.picasso.Picasso
 import com.udacity.asteroidradar.AsteroidAdapter
+import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.R
 import com.udacity.asteroidradar.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private val adapter = AsteroidAdapter()
 
     private val viewModel: MainViewModel by lazy {
         val viewModelFactory = MainViewModelFactory(requireActivity().application)
@@ -19,23 +22,36 @@ class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = FragmentMainBinding.inflate(inflater)
+        binding = FragmentMainBinding.inflate(inflater)
         binding.lifecycleOwner = this
-
         binding.viewModel = viewModel
-
-        val adapter = AsteroidAdapter()
         binding.asteroidRecycler.adapter = adapter
 
         setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         // observe database changes
         viewModel.asteroidList.observe(viewLifecycleOwner, Observer { asteroids ->
             adapter.submitList(asteroids)
         })
-        viewModel.pictureOfDay.observe(viewLifecycleOwner, Observer {
-            Log.i("nasa.main.pic", it.toString())
+        viewModel.pictureOfDay.observe(viewLifecycleOwner, Observer {picture ->
+            picture?.let {
+                if (picture.mediaType == "image") {
+                    loadPictureOfDay(picture)
+                }
+            }
         })
-        return binding.root
+    }
+
+    private fun loadPictureOfDay(pictureOfDay: PictureOfDay) {
+        Picasso.with(binding.activityMainImageOfTheDay.context)
+            .load(pictureOfDay.url)
+            .placeholder(R.drawable.loading_img)
+            .error(R.drawable.ic_broken_image)
+            .into(binding.activityMainImageOfTheDay)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
