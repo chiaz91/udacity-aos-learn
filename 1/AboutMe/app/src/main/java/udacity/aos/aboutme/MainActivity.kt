c38448edbde0be0dba186fe5df71ea3fab7ff0b6
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 20:43:52 +0800

    data binding 2

--- 1/AboutMe/app/src/main/java/udacity/aos/aboutme/MainActivity.kt
+++ 1/AboutMe/app/src/main/java/udacity/aos/aboutme/MainActivity.kt
@@ -10,10 +10,12 @@ import udacity.aos.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName:MyName = MyName("Chia Yu")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
         binding.doneButton.setOnClickListener{
             addNickName(it)
         }
@@ -21,13 +23,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickName(view: View){
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
         }
-
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ 1/AboutMe/app/src/main/java/udacity/aos/aboutme/MyName.kt
@@ -0,0 +1,3 @@
+package udacity.aos.aboutme
+
+data class MyName(var name:String ="", var nickname:String ="")
