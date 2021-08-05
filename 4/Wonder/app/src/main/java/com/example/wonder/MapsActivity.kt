fba68c281f548843106eb068af3aabba8c41fdaf
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-06 00:26:30 +0800

    add marker with long click

--- 4/Wonder/app/src/main/java/com/example/wonder/MapsActivity.kt
+++ 4/Wonder/app/src/main/java/com/example/wonder/MapsActivity.kt
@@ -11,6 +11,7 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -50,7 +51,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val ntu = LatLng(latitude, longitude)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(ntu, zoomLevel))
         map.addMarker(MarkerOptions().position(ntu).title("Marker in NTU"))
+
+        setMapLongClick(map)
     }
+
+    private fun setMapLongClick(map:GoogleMap) {
+        map.setOnMapLongClickListener { latLng ->
+            // A Snippet is Additional text that's displayed below the title.
+            val snippet = String.format(
+                Locale.getDefault(),
+                "Lat: %1$.5f, Long: %2$.5f",
+                latLng.latitude,
+                latLng.longitude
+            )
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(getString(R.string.dropped_pin))
+                    .snippet(snippet)
+            )
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
