59c7c99142b20f8f9799d593cfde1b9043721266
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 23:50:29 +0800

    preparing necessary variables for clipped example

--- 3/ClippingExample/app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ 3/ClippingExample/app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.android.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -9,4 +11,26 @@ class ClippedView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+    private val path = Path()
+    // variables for dimensions for a clipping rectangle around the whole set of shapes.
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+    // circle to be drawn inside the rectangle
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+    // text to be drawn inside the rectangle
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    
 }
