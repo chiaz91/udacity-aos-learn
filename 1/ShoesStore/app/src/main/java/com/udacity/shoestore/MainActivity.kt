dc97f8b4170bf17229bf103830eb9a7bb6c6c5ee
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 21:32:50 +0800

    main activity binding and up button

--- 1/ShoesStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ 1/ShoesStore/app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,17 +2,29 @@ package com.udacity.shoestore
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         Timber.plant(Timber.DebugTree())
 
-        val toolBar = findViewById<Toolbar>(R.id.toolbar)
-        setSupportActionBar(toolBar)
+        setSupportActionBar(binding.toolbar)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
