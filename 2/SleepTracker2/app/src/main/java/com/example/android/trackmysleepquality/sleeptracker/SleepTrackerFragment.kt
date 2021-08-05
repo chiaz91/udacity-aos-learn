7f69d369e225fc07d1af05ab4f605571510954f6
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:13:57 +0800

    recycler view header span entire row

--- 2/SleepTracker2/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ 2/SleepTracker2/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -66,6 +66,13 @@ class SleepTrackerFragment : Fragment() {
 
         // set up recycler view
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup(){
+            override fun getSpanSize(position: Int): Int = when(position){
+                0->3
+                else -> 1
+            }
+
+        }
         val adapter = SleepNightAdapter(SleepNightListener { nightId ->
             sleepTrackerViewModel.onSleepNightClicked(nightId)
         })
