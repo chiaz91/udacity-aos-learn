14791c9abd631c696faf80aa8d059c9d60d57dbf
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:57:27 +0800

    retrofit adding apiKeyInterceptor

--- 2/AsteroidRadar/app/src/main/java/com/udacity/asteroidradar/api/Service.kt
+++ 2/AsteroidRadar/app/src/main/java/com/udacity/asteroidradar/api/Service.kt
@@ -5,6 +5,9 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import com.udacity.asteroidradar.Constants
 import kotlinx.coroutines.Deferred
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -16,30 +19,53 @@ interface AsteroidService {
     @GET("/neo/rest/v1/feed")
     fun getAsteroidListAsync(
         @Query("start_date") startDate: String,
-        @Query("end_date") endDate: String,
-        @Query("api_key") apiKey: String = NASA_KEY
+        @Query("end_date") endDate: String
     ): Deferred<String>
 
 
     @GET("/planetary/apod")
-    fun getPictureOfTheDayAsync(
-        @Query("api_key") apiKey: String  = NASA_KEY
-    ): Deferred<NetworkPictureOfDay>
+    fun getPictureOfTheDayAsync(): Deferred<NetworkPictureOfDay>
 }
 
-private val moshi = Moshi.Builder()
-    .add(KotlinJsonAdapterFactory())
-    .build()
-
 object AsteroidApi {
-    private val retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(CoroutineCallAdapterFactory())
-        .build()
-
+    private lateinit var retrofit:  Retrofit
     val retrofitService: AsteroidService by lazy {
-        retrofit.create(AsteroidService::class.java)
+        getClient().create(AsteroidService::class.java)
+    }
+
+    fun getClient(): Retrofit{
+        if (!::retrofit.isInitialized) {
+            val okHttpClient = OkHttpClient.Builder()
+                .addInterceptor { apiKeyInterceptor(it) }
+                .build()
+
+            val moshi = Moshi.Builder()
+                .add(KotlinJsonAdapterFactory())
+                .build()
+
+            retrofit = Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .addCallAdapterFactory(CoroutineCallAdapterFactory())
+                .build()
+        }
+        return retrofit
+    }
+
+    private fun apiKeyInterceptor(it: Interceptor.Chain): Response {
+        val originalRequest = it.request()
+        val originalHttpUrl = originalRequest.url()
+
+        val newHttpUrl = originalHttpUrl.newBuilder()
+            .addQueryParameter("api_key", NASA_KEY)
+            .build()
+
+        val newRequest = originalRequest.newBuilder()
+            .url(newHttpUrl)
+            .build()
+
+        return it.proceed(newRequest)
     }
 }
