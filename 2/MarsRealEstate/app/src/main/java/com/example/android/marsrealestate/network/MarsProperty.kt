be1adeb9067bbe512b1c4722c6b89beb7ab27eb7
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:22:05 +0800

    Parcelable implementation

--- 2/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ 2/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,10 +17,41 @@
 
 package com.example.android.marsrealestate.network
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.squareup.moshi.Json
 
 data class MarsProperty(
-    val id: String,
-    @Json(name = "img_src") val imgSrcUrl: String,
-    val type: String,
-    val price: Double)
+    val id: String?,
+    @Json(name = "img_src") val imgSrcUrl: String?,
+    val type: String?,
+    val price: Double): Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readDouble()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(id)
+        parcel.writeString(imgSrcUrl)
+        parcel.writeString(type)
+        parcel.writeDouble(price)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MarsProperty> {
+        override fun createFromParcel(parcel: Parcel): MarsProperty {
+            return MarsProperty(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MarsProperty?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
