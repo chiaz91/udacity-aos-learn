9f557c194db72e8ce5c3ddb09f06629690925a6a
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 22:22:48 +0800

    kotlin extension Parcelize

--- 2/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
+++ 2/MarsRealEstate/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -17,41 +17,13 @@
 
 package com.example.android.marsrealestate.network
 
-import android.os.Parcel
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class MarsProperty(
-    val id: String?,
-    @Json(name = "img_src") val imgSrcUrl: String?,
-    val type: String?,
-    val price: Double): Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readString(),
-        parcel.readDouble()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(imgSrcUrl)
-        parcel.writeString(type)
-        parcel.writeDouble(price)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<MarsProperty> {
-        override fun createFromParcel(parcel: Parcel): MarsProperty {
-            return MarsProperty(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MarsProperty?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+    val id: String,
+    @Json(name = "img_src") val imgSrcUrl: String,
+    val type: String,
+    val price: Double): Parcelable
