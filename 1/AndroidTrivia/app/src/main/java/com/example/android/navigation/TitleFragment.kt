8c69f369d21038a3836052d56cc68801d6c9b18b
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 20:57:43 +0800

    navigate with action

--- 1/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 1/AndroidTrivia/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment: Fragment() {
@@ -16,6 +18,20 @@ class TitleFragment: Fragment() {
         val binding: FragmentTitleBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_title, container, false
         )
+        /*
+        binding.playButton.setOnClickListener{ view:View->
+            // [1] navigate to play fragment
+            // Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+            // [2] rewrite with using kotlin extension function
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+        */
+
+        // [3] using Navigation to create OnClickListener
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
         return binding.root
     }
+
 }
