50b0295c8d320b17c0e5b5efea812bdb20c96d99
CY-DESKTOP\chiayu <chiayu.dev@gmail.com>
2021-08-05 21:28:21 +0800

    create fragments

---
+++ 1/ShoesStore/app/src/main/java/com/udacity/shoestore/fragments/InstructionFragment.kt
@@ -0,0 +1,25 @@
+package com.udacity.shoestore.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+
+
+class InstructionFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+}
